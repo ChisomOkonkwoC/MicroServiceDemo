d83137dd70bebad58e16bc726c850a69972cc18c
ChisomOkonkwoC <chisom.okonkwo@decagon.dev>
2022-07-20 16:07:52 +0100

    Refactor RabbitMq Bus and the dependency injections

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,20 +1,27 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MicroRabbit.Infra.IoC
 {
-    public class DependencyContainer
+    public  class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services)
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
